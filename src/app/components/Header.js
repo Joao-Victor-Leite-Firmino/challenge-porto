2b30e2ffcf543bad2b76b2eb36e45e4e015ffd99
João Victor Leite Firmino <jvmaster2@gmail.com>
2023-09-08 15:18:42 -0300

    Imagem caminhão adicionada

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+'use client'
+import styled from 'styled-components'
+
+const Header = styled.header`
+        background-color: blue;
+        color: white;
+        padding: 1em;
+        text-align: center;
+        `;
+
+export default function Cabecalho(props){
+    
+}
