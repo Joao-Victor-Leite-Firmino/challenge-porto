488e08429b71ea3444f5262cca435e2c6e2f4b67
João <jvmaster2@gmail.com>
2023-09-11 22:32:25 -0300

    Botão chamada adicionado e css alterado

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,21 @@
 import './globals.css'
-import ButtonList from "./components/Botaoassistencia";
-
-
+import BotaoChamada from './components/BotaoChamada'
 
 export default function Home() {
   return (
     <main>
       <header class='cabecalho'>
-      <img class='logo' src="porto-seguro-logo (1).png" alt="Logo Porto Seguro"/>
+        <img class='logo' src="porto-seguro-logo (1).png" alt="Logo Porto Seguro"/>
+        <link></link>
       </header>
       <div class='h1'>
-      <h1 class="titulo">Que tipo de assistência você está procurando?</h1>
+        <h1 class="titulo">Olá usuário, selecione abaixo o veículo que precisa de assistência</h1>
       </div>
-      <ButtonList/>
+      <div class="veiculos">
+        <p class="paragrafo">Vigência de 01/01/2023 a 31/12/2024</p>
+        <BotaoChamada 
+        src="/ferramentinha.svg"/>
+      </div>
     </main>
   )
 }
